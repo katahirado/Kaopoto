b4172bbe3a2fbc72779ad19c757fa314872b28b2
katahirado <yuichi@katahirado.jp>
2012-05-23 14:12:47 +0900

    add util method

--- src/jp/katahirado/android/kaopoto/KaopotoUtil.java
+++ src/jp/katahirado/android/kaopoto/KaopotoUtil.java
@@ -10,4 +10,13 @@ public class KaopotoUtil {
         return Const.FACEBOOK_MOBILE_URL+Const.PROF_URL + id;
     }
 
+    public static int stringToTypeID(String object_type) {
+        int result = 0;
+        if(object_type.equals(Const.STREAM)){
+            result= Const.STREAM_ID;
+        }else if(object_type.equals(Const.EVENT)){
+            result= Const.EVENT_ID;
+        }
+        return result;
+    }
 }
