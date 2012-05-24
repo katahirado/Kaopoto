d380f55a9fbb4209551dd8786939b4180ded0667
katahirado <yuichi@katahirado.jp>
2012-05-24 17:55:14 +0900

    イベント詳細マッピング用データを用意

---
+++ src/jp/katahirado/android/kaopoto/model/EventData.java
@@ -0,0 +1,12 @@
+package jp.katahirado.android.kaopoto.model;
+
+import org.json.JSONObject;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Author: yuichi_katahira
+ */
+public class EventData {
+    public EventData(JSONObject jsonObject) {
+    }
+}
