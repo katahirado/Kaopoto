f07261176847f3861ebc49345cf46338a3f25a5f
katahirado <yuichi@katahirado.jp>
2012-05-28 12:50:35 +0900

    画像取得、表示のためにCommentsListAdapterの親クラスをBaseAdapterに変更

--- src/jp/katahirado/android/kaopoto/adapter/CommentsListAdapter.java
+++ src/jp/katahirado/android/kaopoto/adapter/CommentsListAdapter.java
@@ -4,9 +4,13 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
+import com.facebook.android.FriendsGetProfilePics;
+import com.facebook.android.Utility;
 import jp.katahirado.android.kaopoto.R;
+import jp.katahirado.android.kaopoto.activity.PostItemActivity;
 import jp.katahirado.android.kaopoto.model.CommentData;
 
 import java.util.ArrayList;
@@ -15,14 +19,37 @@ import java.util.ArrayList;
  * Created with IntelliJ IDEA.
  * Author: yuichi_katahira
  */
-public class CommentsListAdapter extends ArrayAdapter<CommentData> {
+public class CommentsListAdapter extends BaseAdapter {
     private LayoutInflater layoutInflater;
+    private ImageView image;
     private TextView name;
     private TextView message;
+    private ArrayList<CommentData> commentsList;
+    private PostItemActivity activity;
 
-    public CommentsListAdapter(Context context, ArrayList<CommentData> comments) {
-        super(context, 0, comments);
+    public CommentsListAdapter(PostItemActivity context, ArrayList<CommentData> comments) {
+        if (Utility.model == null) {
+            Utility.model = new FriendsGetProfilePics();
+        }
+        activity = context;
+        Utility.model.setListener(this);
+        commentsList = comments;
         layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
+    @Override
+    public int getCount() {
+        return commentsList.size();
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
     }
 
     @Override
@@ -30,22 +57,19 @@ public class CommentsListAdapter extends ArrayAdapter<CommentData> {
         View view = convertView;
 
         if (convertView == null) {
-            view = layoutInflater.inflate(R.layout.comment_row,null);
+            view = layoutInflater.inflate(R.layout.comment_row, null);
         }
 
-        CommentData commentData = this.getItem(position);
+        CommentData commentData = commentsList.get(position);
         if (commentData != null) {
+            String fromPic = activity.getImageURLFromDB(commentData.getFromUser().getUid());
+            image = (ImageView) view.findViewById(R.id.comment_row_image);
+            image.setImageBitmap(Utility.model.getImage(commentData.getCommentId(), fromPic));
+
             name = (TextView) view.findViewById(R.id.comment_row_name);
             name.setText(commentData.getFromUser().getName());
             message = (TextView) view.findViewById(R.id.comment_row_message);
             message.setText(commentData.getMessage());
-//            screenName = (TextView) view.findViewById(R.id.row_screen_name);
-//            screenName.setText(status.getUser().getScreenName());
-//            createdAt = (TextView) view.findViewById(R.id.row_created_at);
-//            formatDateText = new SimpleDateFormat("yyyy年MM月dd日HH時mm分").format(status.getCreatedAt());
-//            createdAt.setText(" " + formatDateText);
-//            tweetText = (TextView) view.findViewById(R.id.row_tweet_text);
-//            tweetText.setText(status.getText());
         }
         return view;
     }
