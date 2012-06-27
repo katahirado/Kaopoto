377d1904807dca16f48197b0fb795fde1c5c6f52
katahirado <yuichi@katahirado.jp>
2012-06-27 10:46:42 +0900

    更新月日をListViewで表示する部分を追加

--- src/jp/katahirado/android/kaopoto/adapter/InBoxAdapter.java
+++ src/jp/katahirado/android/kaopoto/adapter/InBoxAdapter.java
@@ -3,6 +3,9 @@ package jp.katahirado.android.kaopoto.adapter;
 import android.app.Activity;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import jp.katahirado.android.kaopoto.KaopotoUtil;
 import jp.katahirado.android.kaopoto.R;
 import jp.katahirado.android.kaopoto.model.MessageThreadData;
 
@@ -15,6 +18,8 @@ import java.util.ArrayList;
 public class InBoxAdapter extends FacebookBaseAdapter {
 
     private ArrayList<MessageThreadData> messageThreadList;
+    private MessageThreadData messageThreadData;
+    private TextView updatedTimeText;
 
     public InBoxAdapter(Activity context, ArrayList<MessageThreadData> list) {
         super(context);
@@ -34,9 +39,13 @@ public class InBoxAdapter extends FacebookBaseAdapter {
     @Override
     public View getView(int position, View convertView, ViewGroup viewGroup) {
         View view = convertView;
+        messageThreadData = messageThreadList.get(position);
         if (convertView == null) {
             view = layoutInflater.inflate(R.layout.in_box_row, null);
         }
+        profile_pic = (ImageView) view.findViewById(R.id.picture);
+        updatedTimeText = (TextView) view.findViewById(R.id.updated_time);
+        updatedTimeText.setText(KaopotoUtil.formattedDateString(messageThreadData.getUpdatedTime()));
         return view;
     }
 }
