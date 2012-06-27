3cc98d7e69ee3e2e3466dd81ee8683417f87ca34
katahirado <yuichi@katahirado.jp>
2012-06-27 14:51:08 +0900

    定数追加

--- src/jp/katahirado/android/kaopoto/Const.java
+++ src/jp/katahirado/android/kaopoto/Const.java
@@ -37,4 +37,5 @@ public class Const {
     public static final String FROM = "from";
     public static final String UPDATED_TIME = "updated_time";
     public static final String CREATED_TIME = "created_time";
+    public static final String DATE_FORMAT = "date_format";
 }
