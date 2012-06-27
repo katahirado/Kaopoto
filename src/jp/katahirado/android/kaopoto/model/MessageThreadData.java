f286ae879a0a7362bbbd8608345cf4098a14a631
katahirado <yuichi@katahirado.jp>
2012-06-27 10:45:49 +0900

    MessafeThreadDataモデルを実装

--- src/jp/katahirado/android/kaopoto/model/MessageThreadData.java
+++ src/jp/katahirado/android/kaopoto/model/MessageThreadData.java
@@ -1,12 +1,58 @@
 package jp.katahirado.android.kaopoto.model;
 
+import jp.katahirado.android.kaopoto.Const;
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.Date;
 
 /**
  * Created with IntelliJ IDEA.
  * Author: yuichi_katahira
  */
 public class MessageThreadData {
+    private ArrayList<UserData> toUsers;
+    private ArrayList<CommentData> comments;
+    private Date updatedTime;
+
     public MessageThreadData(JSONObject object) {
+        toUsers = new ArrayList<UserData>();
+        try {
+            JSONArray toUserArray = object.getJSONObject(Const.TO).getJSONArray(Const.DATA);
+            for (int i = 0; i < toUserArray.length(); i++) {
+                toUsers.add(new UserData(toUserArray.getJSONObject(i)));
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        comments = new ArrayList<CommentData>();
+        try {
+            JSONArray cArray = object.getJSONObject(Const.COMMENTS).getJSONArray(Const.DATA);
+            for (int i = 0; i < cArray.length(); i++) {
+                comments.add(new CommentData(cArray.getJSONObject(i)));
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        try {
+            updatedTime = new Date(object.getLong(Const.UPDATED_TIME)
+                    * Const.MILLISECOND + Const.TIMEZONE_OFFSET);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public ArrayList<UserData> getToUsers() {
+        return toUsers;
+    }
+
+    public ArrayList<CommentData> getComments() {
+        return comments;
+    }
+
+    public Date getUpdatedTime() {
+        return updatedTime;
     }
 }
