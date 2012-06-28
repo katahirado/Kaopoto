92ab4e065fcafa370ff0fe80f4e0a93c60231b84
katahirado <yuichi@katahirado.jp>
2012-06-28 22:51:23 +0900

    メッセージ送信がGraph APIでまだ送れないようなので一旦オミット

--- src/jp/katahirado/android/kaopoto/activity/MessageThreadActivity.java
+++ src/jp/katahirado/android/kaopoto/activity/MessageThreadActivity.java
@@ -2,9 +2,6 @@ package jp.katahirado.android.kaopoto.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ListView;
 import jp.katahirado.android.kaopoto.Const;
 import jp.katahirado.android.kaopoto.R;
@@ -22,20 +19,22 @@ import java.util.ArrayList;
  * Created with IntelliJ IDEA.
  * Author: yuichi_katahira
  */
-public class MessageThreadActivity extends Activity
-        implements View.OnClickListener {
-    private EditText messageText;
+public class MessageThreadActivity extends Activity {
+    //        implements View.OnClickListener {
+//    private EditText messageText;
     private MessageThreadData messageThreadData;
     private MessageListAdapter adapter;
     public ProfilesDao profilesDao;
+//    private String message;
+//    private ProgressDialog dialog;
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.message_thread);
 
         ListView commentsListView = (ListView) findViewById(R.id.comments_listview);
-        messageText = (EditText) findViewById(R.id.message_text);
-        Button button = (Button) findViewById(R.id.message_button);
+//        messageText = (EditText) findViewById(R.id.message_text);
+//        Button button = (Button) findViewById(R.id.message_button);
         profilesDao = new ProfilesDao(new DBOpenHelper(this).getReadableDatabase());
 
         try {
@@ -47,17 +46,43 @@ public class MessageThreadActivity extends Activity
         ArrayList<CommentData> commentLists = new ArrayList<CommentData>();
         commentLists.add(new CommentData(messageThreadData));
         commentLists.addAll(messageThreadData.getComments());
-        adapter = new MessageListAdapter(this,commentLists);
+        adapter = new MessageListAdapter(this, commentLists);
         commentsListView.setAdapter(adapter);
 
-        button.setOnClickListener(this);
+//        button.setOnClickListener(this);
     }
 
-    @Override
-    public void onClick(View view) {
-        switch (view.getId()) {
-            case R.id.message_button:
-                break;
-        }
-    }
+//    @Override
+//    public void onClick(View view) {
+//        switch (view.getId()) {
+//            case R.id.message_button:
+//                message = messageText.getText().toString();
+//                if (message.isEmpty()) {
+//                    return;
+//                }
+//                dialog = ProgressDialog.show(this, "",
+//                        getString(jp.katahirado.android.kaopoto.R.string.loading), true, true);
+//                Bundle params = new Bundle();
+//                params.putString(Const.MESSAGE, message);
+//                Utility.mAsyncRunner.request(messageThreadData.getThreadId() + "/" + Const.COMMENTS, params,
+//                        Const.POST, new BaseRequestListener() {
+//                    @Override
+//                    public void onComplete(final String response, final Object state) {
+//                        runOnUiThread(new Runnable() {
+//                            @Override
+//                            public void run() {
+//                                try {
+//                                    adapter.add(new CommentData(new JSONObject(response)));
+//                                } catch (JSONException e) {
+//                                    e.printStackTrace();
+//                                }
+//                                messageText.setText("");
+//                                dialog.dismiss();
+//                            }
+//                        });
+//                    }
+//                }, null);
+//                break;
+//        }
+//    }
 }
